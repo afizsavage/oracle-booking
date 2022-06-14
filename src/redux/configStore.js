46792056e62a54d9fcbfad9ff204e2b87ec15e85
cwaku <rickymormor1@gmail.com>
2022-06-14 12:20:50 +0000

    👌 IMPROVE: little changes

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit/query/react';
+import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import apiSlice from '../features/api/apiSlice';
 
