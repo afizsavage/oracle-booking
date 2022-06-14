051c613d877fde21acdb387ed6a5540c282dfd97
Afiz Savage <savageafiz9@gmail.com>
2022-06-14 12:08:51 +0000

    fix linters error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
-import SideBar from './components/sidebar/index';
+import SideBar from './components/sidebar/sidebar';
 import Home from './pages/home';
 
 const App = () => {
