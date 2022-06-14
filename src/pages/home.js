4bb7a6939d021baa6059e40fe04dc43455851ef7
Afiz Savage <savageafiz9@gmail.com>
2022-06-14 19:38:31 +0000

    style carousel

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,17 @@
 import Carousel from '../components/Carousel';
 
 const Home = () => (
-  <section className="relative w-full pt-5 md:p-0 lg:flex justify-center items-center overflow-y-auto h-screen">
-    <div className="w-full text-center lg:flex flex-col justify-center h-5/6 ">
-      <div className="mb-7 lg:mb-14">
-        <h1 className="font-bold text-xl md:text-3xl md:mb-2 uppercase text-gray-700">
-          Latest Models
-        </h1>
-        <p className="font-semibold text-xs lg:text-sm text-gray-500">
-          Please select a scooter!
-        </p>
+  <section className="relative pt-5 md:p-0 lg:flex justify-center items-center lg:w-10/12 overflow-y-auto h-screen">
+    <div className="w-full text-center lg:flex flex-col justify-center h-full ">
+      <div className="absolute top-10 w-full">
+        <div className="w-full">
+          <h1 className="font-extrabold text-xl md:text-3xl md:mb-2 uppercase text-gray-700">
+            Latest Models
+          </h1>
+          <p className="font-bold text-xs lg:text-sm text-gray-400">
+            Please select a Car
+          </p>
+        </div>
       </div>
       <Carousel />
     </div>
