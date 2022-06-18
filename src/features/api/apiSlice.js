175f818dbfd2c6588ff5fcd12327820c7efa7a4c
cwaku <rickymormor1@gmail.com>
2022-06-18 16:53:44 +0000

    ✅ FEAT: add mutations for authentication

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://gentle-springs-58920.herokuapp.com/api/v2' }),
-  tagTypes: ['Cars', 'Users', 'Favorites'],
+  tagTypes: ['Cars', 'Favorites'],
   endpoints: (builder) => ({}),
 });
 
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import apiSlice from '../features/api/apiSlice';
+import { authApi } from '../features/api/authApi';
 
 const store = configureStore({
   reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+    .concat(apiSlice.middleware, authApi.middleware),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
